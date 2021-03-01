5d00d07887fa57f0e40c821d378c31926792e198
Sanyi Tóth <sandor.toth@hotmail.com>
2021-03-01 20:47:06 +0100

    with async await

--- scripts/post.js
+++ scripts/post.js
@@ -1,32 +1,79 @@
-let blogDetails = document.getElementById("blog");
-let locationHash = Number.parseInt(location.hash.slice(1))
+const locationHash = Number.parseInt(location.hash.slice(1));
+const commentList = document.getElementById("comment-list");
+const blogContent = document.getElementById("blog-content");
+const header = document.querySelector("header");
 
-let getPost = (resource) => {
-    return new Promise((resolve, reject) => {
-        const request = new XMLHttpRequest();
-        request.addEventListener("readystatechange", () => {
-            if (request.readyState === 4 && request.status === 200) {
-                let data = JSON.parse(request.responseText);
-                resolve(data);
-            } else if (request.readyState === 4 && request.status === 404) {
-                reject("error:data fetch failed");
-            }
-        })
-        request.open('GET', resource);
-        request.send();
+
+function logBlog(data1) {
+    data1.forEach((blog) => {
+        if (locationHash === blog.id) {
+            header.innerHTML = `<h1>${blog.title}</h1>
+                                            <div id="box">
+                                              <p>Author:<span>Leanne Graham</span></p>
+                                              <p>website:<span>hildegard.org</span></p>
+                                              <a href="index.html" > << All posts</a> 
+                                            </div> `
+            blogContent.innerHTML = `<p>${blog.body}</p>`
+        }
     })
 }
 
-let getPostPromise = getPost("https://jsonplaceholder.typicode.com/posts")
+function logComments(data2) {
+    data2.forEach((comment) => {
+        let newComment = `<li class="comment-list__item"><h5>${comment.name}</h5>
+                                   <p>${comment.body}</p> 
+                              </li>`
+        commentList.innerHTML += newComment;
+    })
+}
 
-getPostPromise.then(data => {
-        data.forEach((post) => {
-            if (locationHash === post.id) {
-                blogDetails.innerHTML = `<h1>${post.title}</h1>
-                                           <p>${post.body}</p>`
-            }
+async function getBlogAndComments() {
+    let response1 = await fetch("https://jsonplaceholder.typicode.com/posts");
+    let data1 = await response1.json();
+    logBlog(data1);
+
+    let response2 = await fetch(`https://jsonplaceholder.typicode.com/comments?postId=${locationHash}`);
+    return await response2.json();
+}
+
+getBlogAndComments().then(data2 => {
+    logComments(data2)
+})
+
+
+/*fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(response => {
+        if (!response.ok) throw Error(response.statusText);
+        else return response.json();
+    })
+    .then(data => {
+            let POST_ID;
+            data.forEach((post) => {
+                if (locationHash === post.id) {
+                    POST_ID = post.id;
+                    header.innerHTML = `<h1>${post.title}</h1>
+                                            <div id="box">
+                                              <p>Author:<span>Leanne Graham</span></p>
+                                              <p>website:<span>hildegard.org</span></p>
+                                              <a href="index.html" > << All posts</a> 
+                                            </div> `
+                    blogContent.innerHTML = `<p>${post.body}</p>`
+                }
+            })
+            return fetch(`https://jsonplaceholder.typicode.com/comments?postId=${POST_ID}`);
+        }
+    )
+    .then(data => {
+        return data.json();
+    })
+    .then(response => {
+        response.forEach((comment) => {
+            console.log(comment)
+            let newComment = `<li class="comment-list__item"><h5>${comment.name}</h5>
+                                   <p>${comment.body}</p> 
+                              </li>`
+            commentList.innerHTML += newComment;
         })
-    }
-)
-    .catch(data => console.log("promise was rejected:", data));
+    })
+    .catch(data => console.log("promise was rejected:", data));*/
 
