b0441fb6aeb6f25be84e4166b15806346ccb2de4
Toth <sandor.toth@hotmail.com>
2021-03-05 21:28:21 +0100

    form validation

--- scripts/comment.js
+++ scripts/comment.js
@@ -2,7 +2,11 @@ const form = document.getElementById("form-add-comment");
 const name = document.getElementById("name");
 const email = document.getElementById("email");
 const textArea = document.getElementById("comment");
+const submit = document.getElementById("submit");
+const INPUT_MIN_LENGTH = 6;
+const TEXTAREA_MIN_LENGTH = 30;
 
+console.log(locationHash)
 
 async function postComments(newComment) {
     let response = fetch("http://localhost:3000/comments", {
@@ -20,7 +24,7 @@ async function getComments() {
     return await response.json();
 }
 
-getComments().then(comment=>{
+getComments().then(comment => {
     const newCommentId = (comment[comment.length - 1].id) + 1;
     form.addEventListener("submit", (event) => {
         event.preventDefault();
@@ -35,3 +39,41 @@ getComments().then(comment=>{
         location.reload();
     })
 })
+
+function isInputValid(input) {
+    return input.value.length >= INPUT_MIN_LENGTH;
+}
+
+function isTextAreaValid(textarea) {
+    return textarea.value.length >= TEXTAREA_MIN_LENGTH;
+}
+
+function addClassToInput(input) {
+    if (isInputValid(input)) {
+        input.classList.add("valid");
+        input.classList.remove("invalid");
+    } else {
+        input.classList.add("invalid");
+        input.classList.remove("valid");
+    }
+}
+
+function addClassToTextarea(textarea) {
+    if (isTextAreaValid(textarea)) {
+        textarea.classList.add("valid");
+        textarea.classList.remove("invalid");
+    } else {
+        textarea.classList.add("invalid");
+        textarea.classList.remove("valid");
+    }
+}
+
+form.addEventListener("keyup", (event) => {
+    event.stopPropagation();
+    addClassToInput(name);
+    addClassToInput(email);
+    addClassToTextarea(textArea);
+    submit.disabled = !(isInputValid(name) && isInputValid(email) && isTextAreaValid(textArea));
+})
+
+
