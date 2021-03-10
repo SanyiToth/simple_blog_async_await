c876a433c30dd3f542225ccaf5feed2b57c2fe36
Sanyi Tóth <sandor.toth@hotmail.com>
2021-03-10 21:21:12 +0100

    pagination function added

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,8 @@
 const posts = document.getElementById("grid-container")
 const selector = document.getElementById("selector-post-order")
+const postsPaginate = document.getElementById("paginate")
+const POST_PER_PAGE = 30;
 
-function logPosts(index, post) {
-    if (index < 30) {
-        let newPost = `<div class="grid-item">
-                        <h2>${post.title}</h2> 
-                        <p id="blog-content__paragraph">${post.body}</p>  
-                        <a href="post.html#${post.id}" target="_blank" >Learn more >></a>      
-                       </div>`
-        posts.innerHTML += newPost;
-    }
-}
 
 async function getPosts() {
     let response = await fetch("https://jsonplaceholder.typicode.com/posts");
@@ -18,33 +10,79 @@ async function getPosts() {
 }
 
 getPosts().then(data => {
-    data.forEach((post, index) => {
-        logPosts(index, post);
+    const numberOfPages = Math.ceil(data.length / POST_PER_PAGE)
+    logPaginate(numberOfPages);
+})
+
+async function getPartOfPosts() {
+    let response = await fetch("https://jsonplaceholder.typicode.com/posts?_page=1&_limit=30");
+    return await response.json();
+}
+
+getPartOfPosts().then(data => {
+    data.forEach((post) => {
+        logPosts(post);
     })
+})
+
+function logPosts(post) {
+    let newPost = `<div class="grid-item">
+                        <h2>${post.title}</h2> 
+                        <p id="blog-content__paragraph">${post.body}</p>  
+                        <a href="post.html#${post.id}" target="_blank" >Learn more >></a>      
+                       </div>`
+    posts.innerHTML += newPost;
+}
+
+
+function logPaginate(numberOfPages) {
+    for (let page = 1; page <= numberOfPages; page++) {
+        let newPageLink = `<a class="page" href="">${page}</a>`
+        postsPaginate.innerHTML += newPageLink;
+    }
+}
+
+postsPaginate.addEventListener("click", (event) => {
+    event.preventDefault()
+    if (event.target.classList.contains("page")) {
+        fetch(`https://jsonplaceholder.typicode.com/posts?_page=${event.target.innerText}&_limit=${POST_PER_PAGE}`)
+            .then(response => {
+                return response.json()
+            })
+            .then(data => {
+                posts.innerHTML = "";
+                document.body.scrollTop = 0;
+                data.forEach((post) => {
+                    logPosts(post);
+                })
+            })
+    }
 })
 
 
 selector.addEventListener("change", (event) => {
-    if (event.target.value === "oldest first") {//ascend
+    //ascend
+    if (event.target.value === "oldest first") {
         fetch("https://jsonplaceholder.typicode.com/posts?_sort=id&_order=asc")
             .then(response => {
                 return response.json()
             })
             .then(data => {
-                posts.innerHTML="";
-                data.forEach((post, index) => {
-                    logPosts(index, post);
+                posts.innerHTML = "";
+                data.forEach((post) => {
+                    logPosts(post);
                 })
             })
+        //descend
     } else {
         fetch("https://jsonplaceholder.typicode.com/posts?_sort=id&_order=desc")
             .then(response => {
                 return response.json()
             })
             .then(data => {
-                posts.innerHTML="";
-                data.forEach((post, index) => {
-                    logPosts(index, post);
+                posts.innerHTML = "";
+                data.forEach((post) => {
+                    logPosts(post);
                 })
             })
     }
