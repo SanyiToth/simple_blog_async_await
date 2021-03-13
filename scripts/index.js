f62c6274eefae866a0480dbaf1748762e09886ac
Sanyi Tóth <sandor.toth@hotmail.com>
2021-03-13 17:15:37 +0100

    paginate syncronized with post per page and post order function

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,10 @@
 const posts = document.getElementById("grid-container");
-const selectorPostsOrder = document.getElementById("selector-post-order");
-const selectorPostsPerPage = document.getElementById("selector-post-per-page");
 const postsPaginate = document.getElementById("paginate");
 const postPerPage = document.getElementById("selector-post-per-page");
 const postLength = document.getElementById("selector-posts-length");
-const selectorSection = document.getElementById("selector")
-
+const selectorSection = document.getElementById("selector");
+const orderSelector = document.getElementById("selector-post-order");
+let order = "asc";
 
 function logPosts(post) {
     let newPost = `<div class="grid-item">
@@ -16,73 +15,14 @@ function logPosts(post) {
                        </div>`
     posts.innerHTML += newPost;
 }
-
-
 function logPaginate(numberOfPages) {
     for (let page = 1; page <= numberOfPages; page++) {
         let newPageLink = `<span>${page}</span>`
         postsPaginate.innerHTML += newPageLink;
     }
-    postsPaginate.firstElementChild.classList.add("active")
+    postsPaginate.firstElementChild.classList.add("active");
 }
-
-
-function addActiveToClassList(event) {
-    event.target.classList.add("active");
-    Array.from(postsPaginate.children).forEach(item => {
-        if (item !== event.target) {
-            item.classList.remove("active");
-        }
-    })
-}
-
-
-async function getPosts() {
-    let response = await fetch("https://jsonplaceholder.typicode.com/posts");
-    return await response.json();
-}
-
-function logPerPostsLength(data) {
-    postLength.innerText = `/ ${data.length}`;
-}
-
-getPosts().then(data => {
-    logPerPostsLength(data);
-    const numberOfPages = Math.ceil(data.length / postPerPage.value)
-    logPaginate(numberOfPages);
-})
-
-async function getPartOfPosts() {
-    let response = await fetch(`https://jsonplaceholder.typicode.com/posts?_page=1&_limit=${postPerPage.value}`);
-    return await response.json();
-}
-
-getPartOfPosts().then(data => {
-    data.forEach((post) => {
-        logPosts(post);
-
-    })
-
-})
-
-postsPaginate.addEventListener("click", (event) => {
-    if (event.target.localName === "span") {
-        fetch(`https://jsonplaceholder.typicode.com/posts?_page=${event.target.innerText}&_limit=${postPerPage.value}`)
-            .then(response => {
-                return response.json()
-            })
-            .then(data => {
-                posts.innerHTML = "";
-                document.body.scrollTop = 0;
-                data.forEach((post) => {
-                    logPosts(post);
-                })
-                addActiveToClassList(event);
-            })
-    }
-})
-
-function getActiveSite() {
+function getActivePage() {
     let result = null;
     Array.from(postsPaginate.children).forEach(item => {
         if (item.classList.contains("active")) {
@@ -91,49 +31,81 @@ function getActiveSite() {
     })
     return result;
 }
-
-
-selectorPostsOrder.addEventListener("change", (event) => {
-    //ascend
-    if (event.target.value === "oldest first") {
-        fetch(`https://jsonplaceholder.typicode.com/posts?_sort=id&_order=asc&_page=${getActiveSite()}&_limit=${postPerPage.value}`)
-            .then(response => {
-                return response.json()
-            })
+function setActivePage(event) {
+    event.target.classList.add("active");
+    Array.from(postsPaginate.children).forEach(item => {
+        if (item !== event.target) {
+            item.classList.remove("active");
+        }
+    })
+}
+function logPerPostsLength(data) {
+    postLength.innerText = `/ ${data.length}`;
+}
+async function getPosts() {
+    let response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    return await response.json();
+}
+getPosts().then(data => {
+    logPerPostsLength(data);
+    const numberOfPages = Math.ceil(data.length / postPerPage.value)
+    logPaginate(numberOfPages);
+})
+async function getPartOfPosts(order, page = 1, limit = 10) {
+    let response = await fetch(`https://jsonplaceholder.typicode.com/posts?_sort=id&_order=${order}&_page=${page}&_limit=${limit}`);
+    return await response.json();
+}
+getPartOfPosts().then(data => {
+    data.forEach((post) => {
+        logPosts(post);
+    })
+})
+postsPaginate.addEventListener("click", (event) => {
+    if (event.target.localName === "span") {
+        getPartOfPosts(order, event.target.innerText, postPerPage.value)
             .then(data => {
                 posts.innerHTML = "";
+                document.body.scrollTop = 0;
                 data.forEach((post) => {
                     logPosts(post);
                 })
-            })
-        //descend
-    } else {
-        fetch(`https://jsonplaceholder.typicode.com/posts?_sort=id&_order=desc&_page=${getActiveSite()}&_limit=${postPerPage.value}`)
-            .then(response => {
-                return response.json()
-            })
-            .then(data => {
-                posts.innerHTML = "";
-                data.forEach((post) => {
-                    logPosts(post);
-                })
+                setActivePage(event);
             })
     }
 })
-
-selectorPostsPerPage.addEventListener("change", () => {
-    getPosts().then(data => {
-        logPerPostsLength(data);
-        const numberOfPages = Math.ceil(data.length / postPerPage.value)
-        postsPaginate.innerHTML = "";
-        logPaginate(numberOfPages);
-    })
-    getPartOfPosts().then(data => {
-        posts.innerHTML = "";
-        data.forEach((post) => {
-            logPosts(post);
+selectorSection.addEventListener("change", (event) => {
+    if (event.target.value === "oldest first") {
+        order = "asc";
+        getPartOfPosts(order, getActivePage(), postPerPage.value).then(data => {
+            posts.innerHTML = "";
+            data.forEach((post) => {
+                logPosts(post);
+            })
         })
-    })
+    } else if (event.target.value === "latest first") {
+        order = "desc";
+        getPartOfPosts(order, getActivePage(), postPerPage.value).then(data => {
+            posts.innerHTML = "";
+            data.forEach((post) => {
+                logPosts(post);
+            })
+        })
+    } else {
+        orderSelector.value = "";
+        getPosts().then(data => {
+            logPerPostsLength(data);
+            postsPaginate.innerHTML = "";
+            const numberOfPages = Math.ceil(data.length / postPerPage.value);
+            logPaginate(numberOfPages);
+        })
+        getPartOfPosts("asc", "1", postPerPage.value).then(data => {
+                posts.innerHTML = "";
+                data.forEach((post) => {
+                    logPosts(post);
+                })
+            }
+        )
+    }
 })
 
 
